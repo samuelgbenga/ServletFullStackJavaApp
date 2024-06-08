410ed817c16c9d05e4d8ad5357b10596097e696c
samuelgbenga <samuelgbenga972@gmail.com>
2024-06-09 00:48:26 +0100

    succesfully connected to the database

--- src/main/java/org/Tesing/Main.java
+++ src/main/java/org/Tesing/Main.java
@@ -1,8 +1,30 @@
 package org.Tesing;
 
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        String url = "jdbc:mysql://localhost:3306/person";
+        String user = "samuel";
+        String password = "1234";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url,user,password);
+            System.out.println("connection established: "+ url);
+
+            Statement stmt = con.createStatement();
+            String query = "SELECT * FROM person_info";
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                System.out.println(rs.getString(2) + " " + rs.getString(4));
+            }
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/resources/SQLCHEATSHEET.md
@@ -0,0 +1,4 @@
+### to get the port number 
+- "SHOW VARIABLES LIKE 'port';
+### to get the host name
+- "SHOW VARIABLES LIKE 'hostname';
