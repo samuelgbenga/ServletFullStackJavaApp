2004efecface96c974ebc487caa4c1f199d716fa
samuelgbenga <samuelgbenga972@gmail.com>
2024-06-10 10:00:48 +0100

    new commit

--- src/main/java/org/Tesing/Main.java
+++ src/main/java/org/Tesing/Main.java
@@ -2,21 +2,27 @@ package org.Tesing;
 
 import java.sql.*;
 
+
+// using dependencies
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        String url = "jdbc:mysql://localhost:3306/person";
+        String url = "jdbc:mysql:///person";
         String user = "samuel";
         String password = "1234";
+
+        // connecting to a database using jdbc dependency on maven
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(url,user,password);
             System.out.println("connection established: "+ url);
 
-            Statement stmt = con.createStatement();
             String query = "SELECT * FROM person_info";
+            PreparedStatement stmt = con.prepareStatement(query);
+
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
                 System.out.println(rs.getString(2) + " " + rs.getString(4));
